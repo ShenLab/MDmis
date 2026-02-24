49894d7db38a7bfb6da1611e9057aaa1c04a0f09
Aziz Zafar <82284691+aziz-zafar@users.noreply.github.com>
2026-02-24 16:16:39 -0500

    Change config paths to placeholder values
    Updated directory paths in the configuration to placeholder values.

--- code/config.py
+++ code/config.py
@@ -1,11 +1,11 @@
 config = {
-    "code_dir": "/home/az2798/MDmis_revisions/MDmis/code/", #used as the base code folder that has all the other folders
-    "vault_dir": "/nfs/user/Users/az2798/", #used for most raw data files which are large (can be the same as data_dir)
-    "colabfold_dir": "/nfs/user/Users/dl3738/database/",
-    "data_dir": "/home/az2798/MDmis/data/", #used for reading processed data and storing intermediate files
-    "results_dir": "/home/az2798/MDmis/results/", #used for storing figures
-    "models_dir": "/home/az2798/MDmis/models", #used for saving the .pkl files for the trained RF models
-    "CALVADOS_raw_dir": "/nfs/user/Users/az2798/CALVADOS_runs/", #used for storing generated simulations, specific to mutation type (Pathogenic_High_RMSF, Benign etc.)
-    "CALVADOS_processed_dir": "/nfs/user/Users/az2798/processed_CALVADOS/", #used for storing processed All-atom simulations and features
-    "h5py_path": "/nfs/user/Users/az2798/train_data_all/filtered_feature_all_ATLAS_GPCRmd_IDRome.h5" #contains the tensors of dynamic features of simulations
+    "code_dir": "/path/to/dir", #used as the base code folder that has all the other folders
+    "vault_dir":  "/path/to/dir", #used for most raw data files which are large (can be the same as data_dir)
+    "data_dir":  "/path/to/dir", #used for reading processed data and storing intermediate files
+    "results_dir":  "/path/to/dir", #used for storing figures
+    "models_dir":  "/path/to/dir", #used for saving the .pkl files for the trained RF models
+    "CALVADOS_raw_dir":  "/path/to/dir", #used for storing generated simulations, specific to mutation type (Pathogenic_High_RMSF, Benign etc.)
+    "CALVADOS_processed_dir":  "/path/to/dir", #used for storing processed All-atom simulations and features
+    "h5py_path":  "/path/to/dir", #contains the tensors of dynamic features of simulations
+    "morfchibi_dir": "/path/to/dir" #contains the generated files with MoRF Chibi scores
 }
